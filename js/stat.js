95d3b6cb859b66832647347b8c75e2a2e16bf22e
SovaSovitsa <sovasovitsa@gmail.com>
2020-07-07 22:12:58 +0300

    Делает статистику вертикальной

--- js/stat.js
+++ js/stat.js
@@ -1,23 +1,20 @@
 'use strict';
 
 // Размеры облака
-var CLOUD_WIDTH = 500;
-var CLOUD_HEIGHT = 200;
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
 
 // Положение облака
 var CLOUD_X = 100;
-var CLOUD_Y = 50;
+var CLOUD_Y = 10;
 
 // Отступ
 var GAP = 10;
-var FONT_GAP = 15;
-
-// Ширина колонки с текстом
-var TEXT_WIDTH = 100;
+var FONT_GAP = 16;
 
 // Размеры полосы
-var BAR_HEIGHT = 20;
-var barWidth = CLOUD_WIDTH - GAP - TEXT_WIDTH - GAP;
+var BAR_WIDTH = 40;
+var barHeight = CLOUD_HEIGHT - GAP - (FONT_GAP + GAP) * 3;
 
 // Функция отрисовки облака
 var renderCloud = function (ctx, x, y, color) {
@@ -40,15 +37,18 @@ var getMaxElement = function (arr) {
 
 // Функция вывода статистики
 window.renderStatistics = function (ctx, players, times) {
-  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.3)');
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
   ctx.fillStyle = '#000';
 
+  ctx.fillText('Ура! Вы победили!', CLOUD_X + CLOUD_WIDTH / 4, CLOUD_Y + FONT_GAP + GAP);
+  ctx.fillText('Результаты:', CLOUD_X + CLOUD_WIDTH / 4, CLOUD_Y + GAP + FONT_GAP + GAP + GAP);
+
   var maxTime = getMaxElement(times);
 
   for (var i = 0; i < players.length; i++) {
-    ctx.fillText(players[i], CLOUD_X + GAP, CLOUD_Y + GAP + FONT_GAP + (GAP + BAR_HEIGHT) * i);
-    ctx.fillRect(CLOUD_X + GAP + TEXT_WIDTH, CLOUD_Y + GAP + (GAP + BAR_HEIGHT) * i, (barWidth * times[i]) / maxTime, BAR_HEIGHT);
+    ctx.fillText(players[i], CLOUD_X + 80 + GAP + (20 + BAR_WIDTH) * i, CLOUD_Y + CLOUD_HEIGHT - GAP);
+    ctx.fillRect(CLOUD_X + 80 + GAP + (20 + BAR_WIDTH) * i, CLOUD_Y + CLOUD_HEIGHT - GAP - FONT_GAP - GAP - ((barHeight * times[i]) / maxTime) + GAP, BAR_WIDTH, (barHeight * times[i]) / maxTime);
   }
 };
