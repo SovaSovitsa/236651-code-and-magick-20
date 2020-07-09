606071e369265d86ca6c4fc44c2d2941cd1137cd
SovaSovitsa <sovasovitsa@gmail.com>
2020-07-09 21:09:47 +0300

    Ровняет и красит статистику

--- js/stat.js
+++ js/stat.js
@@ -14,7 +14,7 @@ var FONT_GAP = 16;
 
 // Размеры полосы
 var BAR_WIDTH = 40;
-var barHeight = CLOUD_HEIGHT - GAP - (FONT_GAP + GAP) * 3;
+var barHeight = CLOUD_HEIGHT - GAP - (FONT_GAP + GAP) * 4;
 
 // Функция отрисовки облака
 var renderCloud = function (ctx, x, y, color) {
@@ -42,13 +42,27 @@ window.renderStatistics = function (ctx, players, times) {
 
   ctx.fillStyle = '#000';
 
-  ctx.fillText('Ура! Вы победили!', CLOUD_X + CLOUD_WIDTH / 4, CLOUD_Y + FONT_GAP + GAP);
-  ctx.fillText('Результаты:', CLOUD_X + CLOUD_WIDTH / 4, CLOUD_Y + GAP + FONT_GAP + GAP + GAP);
+  ctx.fillText('Ура! Вы победили!', CLOUD_X + GAP * 3, CLOUD_Y + GAP * 3);
+  ctx.fillText('Результаты:', CLOUD_X + GAP * 3, CLOUD_Y + FONT_GAP + GAP * 3);
 
   var maxTime = getMaxElement(times);
 
+  var columnWidth = CLOUD_WIDTH / players.length;
+  var columnGap = (columnWidth - BAR_WIDTH) / 2;
+
   for (var i = 0; i < players.length; i++) {
-    ctx.fillText(players[i], CLOUD_X + 80 + GAP + (20 + BAR_WIDTH) * i, CLOUD_Y + CLOUD_HEIGHT - GAP);
-    ctx.fillRect(CLOUD_X + 80 + GAP + (20 + BAR_WIDTH) * i, CLOUD_Y + CLOUD_HEIGHT - GAP - FONT_GAP - GAP - ((barHeight * times[i]) / maxTime) + GAP, BAR_WIDTH, (barHeight * times[i]) / maxTime);
+    ctx.fillText(players[i], CLOUD_X + columnGap + (columnGap * 2 + BAR_WIDTH) * i, CLOUD_Y + CLOUD_HEIGHT - GAP);
+
+    // Цвет колонки
+    if (players[i] === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    } else {
+      ctx.fillStyle = 'blue';
+    }
+
+    ctx.fillRect(CLOUD_X + columnGap + (columnGap * 2 + BAR_WIDTH) * i, CLOUD_Y + CLOUD_HEIGHT - GAP - FONT_GAP - GAP - ((barHeight * times[i]) / maxTime) + GAP, BAR_WIDTH, (barHeight * times[i]) / maxTime);
+
+    ctx.fillStyle = '#000';
+    ctx.fillText(Math.round(times[i]), CLOUD_X + columnGap + (columnGap * 2 + BAR_WIDTH) * i, CLOUD_Y + CLOUD_HEIGHT - GAP - ((barHeight * times[i]) / maxTime) - GAP - FONT_GAP);
   }
 };
